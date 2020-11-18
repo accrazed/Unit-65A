4ed4361879921cb68de5a637f2fc843e52f49d71
accrazed <accrazed@gmail.com>
2020-11-17 23:03:13 -0500

    (almost) fully functioning poggy game

--- main.js
+++ main.js
@@ -22,15 +22,28 @@ const UserData = sequelize.define('user_data', {
         type: Sequelize.STRING,
         unique: true,
     },
-    da_user: Sequelize.STRING,
-    osu_user: Sequelize.STRING,
-    poggers_best: Sequelize.INTEGER,
-    pogger_last: Sequelize.INTEGER,
+    da_user: {
+        type: Sequelize.STRING,
+        defaultValue: null
+    },
+    osu_user: {
+        type: Sequelize.STRING,
+        defaultValue: null
+    },
+    poggers_best: {
+        type: Sequelize.INTEGER,
+        defaultValue: null
+    },
+    pogger_last: {
+        type: Sequelize.INTEGER,
+        defaultValue: null
+    },
 });
+
 // Initialize guilds db
 const GuildData = sequelize.define('guild_data', {
     name: Sequelize.STRING,
-    poggers_best: Sequelize.STRING,
+    poggers_best_id: Sequelize.STRING,
     snowflake_id: {
         type: Sequelize.STRING,
         unique: true,
@@ -75,8 +88,12 @@ bot.on('message', async msg => {
         }
 
         if (command === 'prefix') {
-            if (!msg.guild.member(msg.author).hasPermission('MANAGE_MESSAGES'))
-                return msg.reply(`Sorry, you need to have Manage Messages permissions to do this`);
+            if (!msg.guild.member(msg.author).hasPermission('MANAGE_MESSAGES')) {
+                if (args.length > 0)
+                    return msg.reply(`Sorry, you need to have Manage Messages permissions to do this`);
+                return msg.channel.send(`Prefix is \`${cur_guild.prefix}\``);
+            }
+            // If there are any args, then the user wants to change the prefix
             if (args.length > 0) {
                 cur_guild.prefix = args.shift();
                 await cur_guild.save();
@@ -88,38 +105,44 @@ bot.on('message', async msg => {
         if (command === 'poggy') {
             const cur_user = await findorCreateUser(msg);
             const time = Date.now() - msg.createdTimestamp;
-            const last_pog = ((Date.now() - cur_user.pogger_last) / 1000 / 60 / 60).toFixed(1);
+            const last_pog = ((Date.now() - cur_user.pogger_last) / 1000 / 60).toFixed(1);
+            let output = ``;
 
-            // Check if it's been 10 hours since last poggy
-            if (cur_user.pogger_last === null || last_pog >= 0.0) {
-                msg.channel.send(`Woggies! ${time}ms\n`);
+            // Check if it's been 1 hour since last poggy
+            if (last_pog == 'NaN' || last_pog >= 60.0) {
+                output = output.concat(`Woggies! ${time}ms\n`);
+                let cur_record = await UserData.findOne({where: { snowflake_id: cur_guild.poggers_best_id}});
+                
 
                 // New personal best
-                if (cur_user.poggers_best === null || cur_user.poggers_best > time) {
+                if (cur_user.poggers_best == null || cur_user.poggers_best > time) {
                     cur_user.poggers_best = time;
                     cur_user.pogger_last = Date.now();
-                    
+
                     await cur_user.save();
-                    return msg.channel.send(`New personal best: ${time}ms.`);
+                    output = output.concat(`New personal best: ${time}ms!\n`);
                 }
                 // New server record
-                if (cur_guild.poggers_best === null || UserData.findOne({
-                        where: {
-                            snowflake_id: cur_guild.poggers_best
-                        }
-                    }).poggers_best > time) {
-                    cur_guild.poggers_best = cur_user.snowflake_id;
+                if (cur_guild.poggers_best_id == null || cur_record.poggers_best > time) {
+                    cur_guild.poggers_best_id = cur_user.snowflake_id;
 
-                    await cur_user.save();
-                    return msg.channel.send(`New server record set by ${cur_user.da_user}: ${time}ms! `);
+                    await cur_guild.save();
+                    output = output.concat(`New server record set by ${cur_user.da_user}: ${time}ms!\n`);
                 }
+                msg.channel.send(output);
                 return;
             } else {
-                return msg.channel.send(`You can only poggy once every 10 hours.\n Cooldown: ${10.0 - last_pog}hrs`);
+                return msg.channel.send(`You can only poggy once every hour.\nCooldown: ${60 - last_pog}min(s)`);
             }
-
-        } else if (command === 'poggychamps') {
-            msg.channel.send(`Current poggy woggie highscore is set by ${await highscore_ping.get(msg.guild.id)} ms.`);
+        }
+        // TO DO: Make poggychamps give a leaderboard of top scores instead of just the record
+        // Thoughts: more databases to hold sorted arrays of scores, tied to ids, specific to each
+        // guild? Will need to sit on this more. 
+        else if (command === 'poggychamps') {
+            const top = await UserData.findOne({where: {snowflake_id: cur_guild.poggers_best_id}});
+            if (top === null)
+                return msg.channel.send(`There isn't a highscore yet on this server.`); 
+            return msg.channel.send(`Current poggy woggie highscore is set by ${top.da_user} with ${top.poggers_best} ms.`);
         } else if (msg.mentions.has('153562159161278473') &&
             msg.author.id === '160854407020412928' &&
             (msg.content.includes("love you") || msg.content.includes("love u"))) {
